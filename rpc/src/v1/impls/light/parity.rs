zippiehq/parity-ethereum

changed method signature to accept r,s,v components

rpc/src/v1/impls/light/parity.rs
@@ -39,15 +39,14 @@ use v1::helpers::light_fetch::LightFetch;
 use v1::metadata::Metadata;
 use v1::traits::Parity;
 use v1::types::{
-	Bytes, U256, U64, H160, H520, H256, H512, CallRequest,
+	Bytes, U256, U64, H160, H256, H512, CallRequest,
 	Peers, Transaction, RpcSettings, Histogram,
 	TransactionStats, LocalTransactionStatus,
 	BlockNumber, ConsensusCapability, VersionInfo,
 	OperationsInfo, ChainStatus,
-	AccountInfo, HwAccountInfo, Header, RichHeader,
+	AccountInfo, HwAccountInfo, Header, RichHeader, RichBasicAccount,
 };
 use Host;
-use v1::types::RichBasicAccount;
 
 /// Parity implementation for light client.
 pub struct ParityClient {
@@ -424,7 +423,7 @@ impl Parity for ParityClient {
 		Err(errors::light_unimplemented(None))
 	}
 
-	fn verify_signature(&self, is_prefixed: bool, message: Bytes, signature: H520) -> Result<RichBasicAccount> {
-		verify_signature(is_prefixed, message, signature, self.client.signing_chain_id())
+	fn verify_signature(&self, is_prefixed: bool, message: Bytes, r: H256, s: H256, v: u64) -> Result<RichBasicAccount> {
+		verify_signature(is_prefixed, message, v, r, s, self.client.signing_chain_id())
 	}
 }

rpc/src/v1/impls/parity.rs
@@ -40,16 +40,15 @@ use v1::helpers::{self, errors, fake_sign, ipfs, SigningQueue, SignerService, Ne
 use v1::metadata::Metadata;
 use v1::traits::Parity;
 use v1::types::{
-	Bytes, U256, U64, H160, H256, H520, H512, CallRequest,
+	Bytes, U256, U64, H160, H256, H512, CallRequest,
 	Peers, Transaction, RpcSettings, Histogram,
 	TransactionStats, LocalTransactionStatus,
 	BlockNumber, ConsensusCapability, VersionInfo,
 	OperationsInfo, ChainStatus,
-	AccountInfo, HwAccountInfo, RichHeader,
+	AccountInfo, HwAccountInfo, RichHeader, RichBasicAccount,
 	block_number_to_id
 };
 use Host;
-use v1::types::RichBasicAccount;
 
 /// Parity implementation.
 pub struct ParityClient<C, M, U> {
@@ -451,7 +450,7 @@ impl<C, M, U, S> Parity for ParityClient<C, M, U> where
 				.map_err(errors::call)
 	}
 
-	fn verify_signature(&self, is_prefixed: bool, message: Bytes, signature: H520) -> Result<RichBasicAccount> {
-		verify_signature(is_prefixed, message, signature, self.client.signing_chain_id())
+	fn verify_signature(&self, is_prefixed: bool, message: Bytes, r: H256, s: H256, v: u64) -> Result<RichBasicAccount> {
+		verify_signature(is_prefixed, message, v, r, s, self.client.signing_chain_id())
 	}
 }

rpc/src/v1/traits/parity.rs
@@ -22,7 +22,7 @@ use jsonrpc_core::{BoxFuture, Result};
 use jsonrpc_macros::Trailing;
 use v1::types::RichBasicAccount;
 use v1::types::{
-	H160, H256, H512, U256, H520, U64, Bytes, CallRequest,
+	H160, H256, H512, U256, U64, Bytes, CallRequest,
 	Peers, Transaction, RpcSettings, Histogram,
 	TransactionStats, LocalTransactionStatus,
 	BlockNumber, ConsensusCapability, VersionInfo,
@@ -221,6 +221,6 @@ build_rpc_trait! {
 
 		/// ecrecover signature
 		#[rpc(name = "parity_verifySignature")]
-		fn verify_signature(&self, bool, Bytes, H520) -> Result<RichBasicAccount>;
+		fn verify_signature(&self, bool, Bytes, H256, H256, u64) -> Result<RichBasicAccount>;
 	}
 }

zenoofthefuture/spot

rpc/src/v1/impls/light/parity.rs
@@ -36,15 +36,14 @@ use v1::helpers::light_fetch::{LightFetch, light_all_transactions};
 use v1::metadata::Metadata;
 use v1::traits::Parity;
 use v1::types::{
-	Bytes, U256, H64, H160, H520, H256, H512, CallRequest,
+	Bytes, U256, H64, H160, H256, H512, CallRequest,
 	Peers, Transaction, RpcSettings, Histogram,
 	TransactionStats, LocalTransactionStatus,
 	BlockNumber, LightBlockNumber, ConsensusCapability, VersionInfo,
-	OperationsInfo, ChainStatus,
-	AccountInfo, HwAccountInfo, Header, RichHeader, Receipt,
+	OperationsInfo, ChainStatus, Receipt,
+	AccountInfo, HwAccountInfo, Header, RichHeader, RichBasicAccount,
 };
 use Host;
-use v1::types::RichBasicAccount;
 
 /// Parity implementation for light client.
 pub struct ParityClient {
@@ -426,7 +425,7 @@ impl Parity for ParityClient {
 			Err(errors::status_error(has_peers))
 		}
 	}
-	fn verify_signature(&self, is_prefixed: bool, message: Bytes, signature: H520) -> Result<RichBasicAccount> {
-		verify_signature(is_prefixed, message, signature, self.client.signing_chain_id())
+	fn verify_signature(&self, is_prefixed: bool, message: Bytes, r: H256, s: H256, v: u64) -> Result<RichBasicAccount> {
+		verify_signature(is_prefixed, message, v, r, s, self.client.signing_chain_id())
 	}
 }

rpc/src/v1/impls/parity.rs
@@ -43,16 +43,15 @@ use v1::helpers::{self, errors, fake_sign, ipfs, SigningQueue, SignerService, Ne
 use v1::metadata::Metadata;
 use v1::traits::Parity;
 use v1::types::{
-	Bytes, U256, H64, H160, H256, H520, H512, CallRequest,
+	Bytes, U256, H64, H160, H256, H512, CallRequest,
 	Peers, Transaction, RpcSettings, Histogram,
 	TransactionStats, LocalTransactionStatus,
 	BlockNumber, ConsensusCapability, VersionInfo,
 	OperationsInfo, ChainStatus,
-	AccountInfo, HwAccountInfo, RichHeader, Receipt,
+	AccountInfo, HwAccountInfo, RichHeader, Receipt, RichBasicAccount,
 	block_number_to_id
 };
 use Host;
-use v1::types::RichBasicAccount;
 
 /// Parity implementation.
 pub struct ParityClient<C, M, U> {
@@ -506,7 +505,7 @@ impl<C, M, U, S> Parity for ParityClient<C, M, U> where
 		}
 	}
 
-	fn verify_signature(&self, is_prefixed: bool, message: Bytes, signature: H520) -> Result<RichBasicAccount> {
-		verify_signature(is_prefixed, message, signature, self.client.signing_chain_id())
+	fn verify_signature(&self, is_prefixed: bool, message: Bytes, r: H256, s: H256, v: u64) -> Result<RichBasicAccount> {
+		verify_signature(is_prefixed, message, v, r, s, self.client.signing_chain_id())
 	}
 }

rpc/src/v1/traits/parity.rs
@@ -22,7 +22,7 @@ use jsonrpc_core::{BoxFuture, Result};
 use jsonrpc_macros::Trailing;
 use v1::types::RichBasicAccount;
 use v1::types::{
-	H64, H160, H256, H512, U256, H520, Bytes, CallRequest,
+	H160, H256, H512, U256, H64, Bytes, CallRequest,
 	Peers, Transaction, RpcSettings, Histogram,
 	TransactionStats, LocalTransactionStatus,
 	BlockNumber, ConsensusCapability, VersionInfo,
@@ -239,6 +239,6 @@ build_rpc_trait! {
 		fn status(&self) -> Result<()>;
 		/// ecrecover signature
 		#[rpc(name = "parity_verifySignature")]
-		fn verify_signature(&self, bool, Bytes, H520) -> Result<RichBasicAccount>;
+		fn verify_signature(&self, bool, Bytes, H256, H256, u64) -> Result<RichBasicAccount>;
 	}
 }